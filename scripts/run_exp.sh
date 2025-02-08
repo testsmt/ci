b150c02d409bb51da71e5d4b1afb83296e2d14c1
Andrei Zhukov <andrei.zhukov@getyourguide.com>
2025-02-08 15:49:49 +0100

    test

--- scripts/run_exp.sh
+++ scripts/run_exp.sh
@@ -13,21 +13,9 @@ solvers_cfg_path=$3  # Corrected from $5 to $3
 num_cores=4
 result_dir="./results"
 
-
-# Debugging: List contents of the results directory
-echo "Listing contents of $result_dir:"
-ls "$result_dir"
-
-# Debugging: List contents of the specific theory directory
-echo "Listing contents of $result_dir/$theory:"
-ls "$result_dir/$theory"
-
-# Debugging: List contents of the temp directory
-echo "Listing contents of $result_dir/$theory/temp:"
-ls "$result_dir/$theory/temp"
-
 # Iterate over each .smt2 file in the specified directory
 find $result_dir/$theory/temp -name "*.smt2" -print0 | while IFS= read -r -d '' file; do
     echo "Processing file: $file"
     python ./scripts/oracle.py "$file" "$file.time" "$solvers_cfg_path" "$result_dir/$theory/bugs" $timeout $memout
+    sleep 0.1
 done
