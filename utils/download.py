afc1389a0c69103aeb53d14653be74f08504958e
Andrei Zhukov <andrei.zhukov@getyourguide.com>
2025-02-09 21:07:53 +0100

    test

--- utils/download.py
+++ utils/download.py
@@ -28,13 +28,30 @@ def extract_tar_gz(file_path, extract_to='.', rename_to=None):
         new_folder_path = os.path.join(extract_to, rename_to)
         shutil.move(extracted_folder, new_folder_path)
 
+
 def extract_tar_bz2(file_path, extract_to='.', rename_to=None):
     with tarfile.open(file_path, 'r:bz2') as tar_ref:
         tar_ref.extractall(extract_to)
-    if rename_to:
-        extracted_folder = os.path.join(extract_to, os.path.splitext(os.path.basename(file_path))[0])
+
+    extracted_items = os.listdir(extract_to)
+
+    extracted_folder = None
+    for item in extracted_items:
+        item_path = os.path.join(extract_to, item)
+        if os.path.isdir(item_path) and item != rename_to:
+            extracted_folder = item_path
+            break
+
+    if rename_to and extracted_folder:
         new_folder_path = os.path.join(extract_to, rename_to)
+
+        if os.path.exists(new_folder_path):
+            shutil.rmtree(new_folder_path)
+
         shutil.move(extracted_folder, new_folder_path)
+        print(f"Renamed extracted folder to: {new_folder_path}")
+    elif rename_to and not extracted_folder:
+        print("Warning: No extracted folder found to rename.")
 
 def extract_file(file_path, extract_to='.', rename_to=None):
     if file_path.endswith('.zip'):
