8087cce4f31385d1cab0c8c6f5793cbeabe39c66
Andrei Zhukov <andrei.zhukov@getyourguide.com>
2025-02-23 18:44:08 +0100

    test

--- cvc5-nightly/main.py
+++ cvc5-nightly/main.py
@@ -42,7 +42,7 @@ def main():
 
     local_filename = matching_asset['name']
     download_file(matching_asset['browser_download_url'], local_filename)
-    extract_file(local_filename, extract_to='./', rename_to="solver")
+    extract_file(local_filename, extract_to='./', rename_to="solver", folder_prefix="cvc5-Linux-")
     os.chmod(path_to_solver_binary, 0o755)
 
     write_to_file("./solvers.cfg", "./solver/bin/cvc5")
